1af1ca3c86fae497ecd8d416357bf0a48f8f4497
Neal McBurnett <neal@mcburnett.org>
2020-08-14 08:29:56 -0600

    Fix km_factor, fix doctests, clean up code
    drop prints

--- kaplan_markov.py
+++ kaplan_markov.py
@@ -7,13 +7,13 @@ For the formulas and a worked example, see
  Mark Lindeman, 1/10/2010 (v. 1.2x, 3/1/2010)
  https://d56fe2f5-a-62cb3a1a-s-sites.googlegroups.com/site/electionaudits/small-batch/kaplan-example-12x.pdf
 
-The example works with batch data from 2008 election in California’s 3rd Congressional District (CD3), in
-the file ca-cd3-2018-batches.csv
+The example provides partial batch data from 2008 election in California’s
+3rd Congressional District (CD3), in the file ca-cd3-2018-batches.csv
 
 The product of the final column (overall K-M P value) from the bottom of page six is
  0.098616.
-Mine:
- 0.09827592427176036
+It is calculated here more exactly:
+ 0.09861647993802505
 
 This is a subset of the full set of batches, from California's Statewide Database (SWDB).
 To fully replicate the calculations, we'd need that whole dataset.
@@ -30,15 +30,106 @@ from dataclass_csv import DataclassReader
 
 @dataclass
 class VoteCounts:
+    "Ballot counts and votes per choice for a set of ballots"
+
     name: str
     ballotcount: int
     tally: Dict[str, int]
-    # num_winners: int
 
 
+def error_bound(votecounts, winners, margins):
+    """Return the error bound u_p for the given VoteCounts and pairwise margins.
+
+    Example from top of table in page 3 of Lindeman:
+    >>> error_bound(VoteCounts("060031A", 139, {"Lungren": 48, "Durston": 83}), ["Lungren"], {"Lungren:Durston": 17453})
+    0.005958860940812468
+
+    >>> error_bound(VoteCounts("0606726420", 382, {"Lungren": 158, "Durston": 172}), ["Lungren"], {"Lungren:Durston": 17453})
+    0.021085200252105654
+
+    >>> error_bound(VoteCounts(
+    ... "0606726420",
+    ... 382,
+    ... {"Lungren": 158, "Durston": 172, "Tuma": 14, "Padilla": 15}),
+    ... ["Lungren"],
+    ... {'Lungren:Durston': 17453, 'Lungren:Padilla': 142046, 'Lungren:Tuma': 148151})
+    0.021085200252105654
+
+    >>> error_bound(VoteCounts("b1", 100, {"A": 60, "B": 20}), ["Elvis"], 200)
+    Traceback (most recent call last):
+    ValueError: Set of winners '['Elvis']' is not subset of candidates in tally: dict_keys(['A', 'B'])
+    """
+
+    candidates = set(votecounts.tally)
+
+    if not set(winners).issubset(candidates):
+        raise ValueError(
+            f"Set of winners '{winners}' is not subset of candidates in tally: {votecounts.tally.keys()}"
+        )
+
+    losers = candidates - set(winners)
+
+    bp = votecounts.ballotcount
+
+    return max(
+        (bp + votecounts.tally[winner] - votecounts.tally[loser])
+        / margins[f"{winner}:{loser}"]
+        for winner in winners
+        for loser in losers
+    )
+
+
+def relative_error(reported_votecounts, audit_votecounts, winners, margins):
+    """Return the relative error e_p for the given VoteCounts
+
+    Example from page 4 of Lindeman, precinct 0606726420:
+    >>> relative_error(
+    ...    VoteCounts("0606726420", 424, {"Lungren": 158, "Durston": 172}),
+    ...    VoteCounts("0606726420", 424, {"Lungren": 158, "Durston": 182}),
+    ...    ["Lungren"],
+    ...    {"Lungren:Durston": 17453})
+    0.000572967398155045
+
+    Example from table on page 6 of Lindeman, precinct 06009350
+    >>> relative_error(
+    ...    VoteCounts("06009350", 919, {"Lungren": 440, "Durston": 392}),
+    ...    VoteCounts("06009350", 919, {"Lungren": 442, "Durston": 393}),
+    ...    ["Lungren"],
+    ...    {"Lungren:Durston": 17453})
+    -5.72967398155045e-05
+    """
+
+    # Note that we reuse the code in error_bound by inventing a new set
+    # of tallies which are the discrepancies in the reported tallies, and
+    # using a ballotcount of zero.
+
+    discrepancies = {
+        c: reported_votecounts.tally[c] - audit_votecounts.tally[c]
+        for c in reported_votecounts.tally
+    }
+
+    discrepancy_vc = copy.deepcopy(reported_votecounts)
+    discrepancy_vc.tally = discrepancies
+    discrepancy_vc.ballotcount = 0
+
+    return error_bound(discrepancy_vc, winners, margins)
+
+
+def km_factor(U, taint):
+    """Return the Kaplan-Markov factor for a given value of U and taint.
+
+    Example from page 5 of Lindeman
+    >>> km_factor(20.47, 0.003953)
+    0.9549228314475828
+    """
+
+    return (1.0 - (1.0 / U)) / (1.0 - taint)
+
+
+# This should go in a test.py file
 @dataclass
 class ContestBatchRow:
-    "Class to represent rows in ca-cd3-2018-batches.csv"
+    "Represent rows in Lindeman test data from ca-cd3-2018-batches.csv"
 
     precinct: str
     ballots_cast: int
@@ -59,132 +150,33 @@ class ContestBatchRow:
     net_KM_factor: float
 
 
-# FIXME: needs array of margins per pair....
-def error_bound(votecounts, winners, margins):
-    """Return the error bound u_p for the given VoteCounts and pairwise margins.
-
-    Example from top of table in page 3 of Lindeman:
-    >>> error_bound(VoteCounts("060031A", 139, {"Lungren": 48, "Durston": 83}), ["Lungren"], 17453)
-    0.005958860940812468
-
-    >>> error_bound(VoteCounts("0606726420", 382, {"Lungren": 158, "Durston": 172}), ["Lungren"], 17453)
-    0.021085200252105654
-
-    >>> error_bound(VoteCounts("0606726420", 382, {"Lungren": 158, "Durston": 172, "Tuma": 14, "Padilla": 15}), ["Lungren"], 17453)
-    0.021085200252105654 (fails because we need actual margins for each pair)
-
-    >>> error_bound(VoteCounts("b1", 100, {"A": 60, "B": 20}), ["Elvis"], 200)
-    Traceback (most recent call last):
-    ValueError: Set of winners '['Elvis']' is not subset of candidates in tally: dict_keys(['A', 'B'])
-    """
-
-    print(margins)
-    candidates = set(votecounts.tally)
-
-    if not set(winners).issubset(candidates):
-        raise ValueError(
-            f"Set of winners '{winners}' is not subset of candidates in tally: {votecounts.tally.keys()}"
-        )
-
-    losers = candidates - set(winners)
-
-    bp = votecounts.ballotcount
-
-    # TODO: Perhaps define function of tally and winners/losers which returns all margins labeled by winner/loser
-    #  Then we can calculate all contest-wide margins, margins per batch, take result and calculate u or ep, etc.
-    #  only if we set up dummy margins of 1
-
-    print(f'm {margins}, t {votecounts.tally}')
-    return max(
-        (bp + votecounts.tally[winner] - votecounts.tally[loser]) / margins[f"{winner}:{loser}"]
-        for winner in winners
-        for loser in losers
-    )
-
-
-# FIXME: needs array of margins per pair....
-def relative_error(reported_votecounts, audit_votecounts, winners, margins):
-    """Return the relative error e_p for the given VoteCounts
-
-    FIXME: need max of absolute values?     Can taint sign be reversed?
-
-    Example from page 4 of Lindeman, precinct 0606726420:
-    >>> relative_error(
-    ...    VoteCounts("0606726420", 424, {"Lungren": 158, "Durston": 172}),
-    ...    VoteCounts("0606726420", 424, {"Lungren": 158, "Durston": 182}),
-    ...    ["Lungren"],
-    ...    17453)
-    0.000572967398155045
-
-    Example from table on page 6 of Lindeman, precinct 06009350
-    >>> relative_error(
-    ...    VoteCounts("06009350", 919, {"Lungren": 440, "Durston": 392}),
-    ...    VoteCounts("06009350", 919, {"Lungren": 442, "Durston": 393}),
-    ...    ["Lungren"],
-    ...    17453)
-    -5.72967398155045e-05
-    """
-
-    # Note that we reuse the code in error_bound by inventing a new set
-    # of tallies which are the discrepancies in the reported tallies, and
-    # using a ballotcount of zero.
-
-    discrepancies = {
-        c: reported_votecounts.tally[c] - audit_votecounts.tally[c]
-        for c in reported_votecounts.tally
-    }
-
-    discrepancy_vc = copy.deepcopy(reported_votecounts)
-    discrepancy_vc.tally = discrepancies
-    discrepancy_vc.ballotcount = 0
-
-    return error_bound(discrepancy_vc, winners, margins)
-
-
-def km_factor(contest, discrepancy, u):
-    """Return the KM factor for a given audit's discrepancy and u, in given contest
-    TODO: add doctest.
-    """
-
-    taint = discrepancy / min(contest.margins.values()) / u
-    return (1.0 - (1.0 / contest.U)) / (1.0 - taint)
-
-
 def lindeman_test():
     # A quick-and-dirty Contest class instance, hardcoded from Lindeman example.
 
     c = types.SimpleNamespace()
 
+    # First need to figure out the minimum margin of all pairs of winners and losers for this contest
+    tally = {"Lungren": 155424, "Durston": 137971, "Tuma": 7273, "Padilla": 13378}
+
     # To calculate U, we need all the batches, not just the selected batches.
     # Take this approximate value from the Lindeman paper
     c.U = 20.47
-    # [May be 20.47011975018621 based on old calculation with all 774 audit units]
+    # Actually 20.47011975018621 based on an old calculation with all 774 audit units
     c.U = 20.47011975018621
 
-    c.choices = "Lungren,Durston,Tuma,Padilla".split(
-        ","
-    )  #  TODO: after fixed to allow more than 2 candidates
-    # c.choices = "Lungren,Durston".split(",")  # after fixed
+    c.choices = tally.keys()
     c.winners = ["Lungren"]
 
     losers = list(set(c.choices) - set(c.winners))  # no list?
-    print(losers)
 
-    # First need to figure out the minumum margin of all pairs of winners and losers for this contest
-    tally = {"Lungren": 155424, "Durston": 137971, "Tuma": 7273, "Padilla": 13378}
-
-    contest_vc = VoteCounts(
-        "CD3",
-        314046,
-        tally)
+    # Based on total votes, but really should dig out total ballots
+    contest_vc = VoteCounts("CD3", 314046, tally)
 
     c.margins = {
         f"{winner}:{loser}": tally[winner] - tally[loser]
         for winner in c.winners
         for loser in losers
     }
-
-    print(c.margins)
 
     min_margin = min(c.margins.values())
 
@@ -195,7 +187,7 @@ def lindeman_test():
     km_p_value = 1.0
 
     print(
-        f"precinct,net_taint,net_discrepancy,reported_taint,calculated_taint,w_p,max_delta_in_batch"
+        f"precinct,taint,net_taint,reported_taint,calculated_taint,w_p,max_delta,net_p_value"
     )
 
     for row in reader:
@@ -208,40 +200,27 @@ def lindeman_test():
         audit_votecounts = VoteCounts(row.precinct, row.ballots_cast, audit_tally)
 
         up = error_bound(reported_votecounts, c.winners, c.margins)
-        ep = relative_error(
-            reported_votecounts, audit_votecounts, c.winners, c.margins
-        )
+        ep = relative_error(reported_votecounts, audit_votecounts, c.winners, c.margins)
         taint = ep / up
 
         taintcheck = abs(taint - row.taint)
 
-        print(
-            f"p: {row.precinct}, up: {up}, ep: {ep}, taint: {taint}, taintcheck: {taintcheck}"
-        )
-
         w_p = getattr(row, "Lungren")
         max_delta_in_batch = min(w_p - getattr(row, loser) for loser in losers)
         u = (row.ballots_cast + row.Lungren - row.Durston) / min_margin
-        d = row.Lungren - row.Durston - (row.audit_Lungren - row.audit_Durston)
 
-        p_value = km_factor(c, d, u)
+        p_value = km_factor(c.U, taint)
         net_p_value = p_value ** row.times_drawn
 
         km_p_value *= net_p_value
 
         print(
-            f" {row.precinct}, {net_p_value}, {d}, {row.taint}, {taint}, km_factor {p_value}, {w_p}, {max_delta_in_batch}, {km_p_value}"
+            f"{row.precinct},{p_value},{net_p_value},{row.taint},{taint},{w_p},{max_delta_in_batch},{km_p_value}"
         )
-        # print(f'{row.precinct},{row.Lungren},{row.KM_factor*2}')
 
-        if taintcheck > 5e-7:
-            print(
-                f" \n ERROR precinct {row.precinct}: taintcheck {taintcheck} > 10**-6: mine: {taint} vs example: {row.taint}, {row}"
-            )
-        # assert taintcheck < 10**-6, f"precinct {row.precinct}: taintcheck {taintcheck} < 10**-6: {taint}, {row.taint}"
-        print(" " + str(row))
-
-    print(f"Overall km_p_value: {km_p_value}")
+        assert (
+            taintcheck < 5e-7
+        ), f"precinct {row.precinct}: {taintcheck} == abs({taint} - {row.taint}) > 5e-7"
 
 
 if __name__ == "__main__":
